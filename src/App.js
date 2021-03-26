f3b653a3627ae48c8d9ac4ebb1837fc2ec3a43d7
Max Upravitelev <max@upravitelev.de>
2021-03-26 11:25:54 +0100

    add scheduler component

--- src/App.js
+++ src/App.js
@@ -11,13 +11,14 @@ import UrlForm from './components/UrlForm'
 import Capture from './components/Cam/Capture'
 import Navigation from './components/Dashboard/Navigation'
 
+import Scheduler from './components/Scheduler'
+
 // import css and material ui components
 import './App.css'
-import Grid from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid'
 
 // use media query to determine screen size
 import useMediaQuery from '@material-ui/core/useMediaQuery'
-
 
 const App = () => {
   // fetch urls from store to check if setting up url is necessary
@@ -27,27 +28,34 @@ const App = () => {
 
   console.log(streamUrl)
   // determine screen size
-  const checkScreenWidth = useMediaQuery('(max-width:600px)');
+  const checkScreenWidth = useMediaQuery('(max-width:600px)')
 
   // return url form if streamUrl is not set yet
   if (!streamUrl)
     return (
-      <Grid container direction="column" alignItems="center" spacing={1} justify="center" >
-        <Grid >
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        spacing={1}
+        justify="center"
+      >
+        <Grid>
           <Header />
         </Grid>
-        <Grid >
+        <Grid>
           <UrlForm />
         </Grid>
-      </Grid >
+        <Grid>
+          <Scheduler />
+        </Grid>
+      </Grid>
     )
-
   else {
     // render main app based on screen size
     if (!checkScreenWidth) {
       return (
         <Grid container spacing={2} justify="center" alignItems="center">
-
           <Grid item xs={4} sm={4} zeroMinWidth>
             <Capture />
             <Navigation />
@@ -71,8 +79,6 @@ const App = () => {
       )
     }
   }
-
-
 }
 
 export default App
---
+++ src/components/Scheduler/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+
+// return header component
+const Scheduler = () => {
+  return (
+    <div className="app">
+      <Typography variant="h5" component="h2">
+        [ schedule visit ]
+      </Typography>
+    </div>
+  )
+}
+
+export default Scheduler
