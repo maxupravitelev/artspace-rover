e06ca56c02ec8ef029fa055d1b517e5ba2b53029
Max Upravitelev <max@upravitelev.de>
2021-03-30 13:08:22 +0200

    move handling urls to Exhibition component

--- src/App.js
+++ src/App.js
@@ -24,19 +24,12 @@ import Exhibitions from './components/Exhibitions'
 import './App.css'
 import Grid from '@material-ui/core/Grid'
 import Container from '@material-ui/core/Container'
-// use media query to determine screen size
 
+// use media query to determine screen size
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 
 const App = () => {
-  // fetch urls from store to check if setting up url is necessary
 
-  let socket = useSelector((state) => state.socket)
-  let streamUrl = useSelector((state) => state.urls.streamUrl)
-
-  // console.log(streamUrl)
-  // determine screen size
-  const checkScreenWidth = useMediaQuery('(max-width:600px)')
 
   return (
     <Container>
@@ -66,62 +59,7 @@ const App = () => {
     </Container>
   )
 
-  // // return url form if streamUrl is not set yet
-  // if (!streamUrl)
-  //   return (
-  //     <div>
-  //     <Grid
-  //       container
-  //       direction="column"
-  //       alignItems="center"
-  //       spacing={1}
-  //       justify="center"
-  //     >
-  //       <Grid>
-  //         <Header />
-  //       </Grid>
-  //       <Grid>
-  //         <UrlForm />
-  //       </Grid>
-  //       <Grid>
-  //         <Scheduler />
-  //       </Grid>
-  //     </Grid>
-  //     <Footer />
-  //     </div>
-  //   )
-  // else {
-  //   // render main app based on screen size
-  //   if (!checkScreenWidth) {
-  //     return (
-  //       <div>
-  //       <Grid container spacing={2} justify="center" alignItems="center">
-  //         <Grid item xs={4} sm={4} zeroMinWidth>
-  //           <Capture />
-  //           <Navigation />
-  //         </Grid>
-
-  //         {/* <Grid item xs={4} sm={4} zeroMinWidth>
-  //           <ConfigList />
-  //         </Grid>
-
-  //         <Grid item xs={4} sm={4} >
-  //           <NavBar />
-  //         </Grid> */}
-  //       </Grid>
-  //       <Footer />
-  //       </div>
-  //     )
-  //   } else {
-  //     return (
-  //       <div>
-  //         <Capture />
-  //         <Navigation />
-  //         <Footer />
-  //       </div>
-  //     )
-  //   }
-  // }
+  
 }
 
 export default App
--- src/components/Exhibitions/Exhibition.js
+++ src/components/Exhibitions/Exhibition.js
@@ -2,15 +2,77 @@ import React from 'react'
 
 import { Typography } from '@material-ui/core'
 
+
+
+// init redux and import reducers
+import { useSelector } from 'react-redux'
+
+// import components
+import UrlForm from '../../components/UrlForm'
+import Capture from '../../components/Cam/Capture'
+import Navigation from '../../components/Dashboard/Navigation'
+import Scheduler from '../../components/Scheduler'
+
+// import material ui components
+import Grid from '@material-ui/core/Grid'
+
+// use media query to determine screen size
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+
 // return header component
 const Exhibition = () => {
-  return (
-    <div className="app">
-      <Typography variant="subtitle2" component="">
-        demo exhibition
-      </Typography>
-    </div>
-  )
+  
+  // fetch urls from store to check if setting up url is necessary
+  let socket = useSelector((state) => state.socket)
+  let streamUrl = useSelector((state) => state.urls.streamUrl)
+
+  // console.log(streamUrl)
+  // determine screen size
+  const checkScreenWidth = useMediaQuery('(max-width:600px)')
+
+  // return url form if streamUrl is not set yet
+  if (!streamUrl)
+    return (
+      <div>
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          spacing={1}
+          justify="center"
+        >
+          <Grid>
+            <UrlForm />
+          </Grid>
+          <Grid>
+            <Scheduler />
+          </Grid>
+        </Grid>
+      </div>
+    )
+  else {
+    // render main app based on screen size
+    if (!checkScreenWidth) {
+      return (
+        <div>
+          <Grid container spacing={2} justify="center" alignItems="center">
+            <Grid item xs={4} sm={4} zeroMinWidth>
+              <Capture />
+              <Navigation />
+            </Grid>
+          </Grid>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Capture />
+          <Navigation />
+          <Footer />
+        </div>
+      )
+    }
+  }
 }
 
 export default Exhibition
