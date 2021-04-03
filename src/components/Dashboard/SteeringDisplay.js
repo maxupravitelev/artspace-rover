00df211c9151751b6499d8395650c105b9d0de36
Max Upravitelev <max@upravitelev.de>
2021-04-03 21:19:39 +0200

    add SteeringDisplay component

---
+++ src/components/Dashboard/SteeringDisplay.js
@@ -0,0 +1,12 @@
+const SteeringDispay = ({  }) => {
+  // const [motorDirection, setMotorDirection] = useState(null)
+
+
+  return (
+    <div className="app">
+        |
+    </div>
+  )
+}
+
+export default SteeringDispay
---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import { Button, Typography } from '@material-ui/core'
+
+import useKeyPress from '../../hooks/useKeyPress'
+ 
+import DirectionDispay from './DirectionDisplay'
+import SteeringDispay from './SteeringDisplay'
+
+import SteeringButtons from './SteeringButtons'
+
+
+
+// return component for navigating rover
+const Dashboard = ({  }) => {
+
+  const left = useKeyPress(37);
+  const up = useKeyPress(38);
+  const right = useKeyPress(39);
+  const down = useKeyPress(40);
+
+
+  return (
+    <div className="app">
+      <DirectionDispay up={up} down={down} left={left} right={right}/>
+      <SteeringDispay />
+      <SteeringButtons />
+
+    </div>
+  )
+}
+
+
+export default Dashboard
