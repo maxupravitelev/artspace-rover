ff25fbbc83060bbba9da325cea92d3089d3332d3
Max Upravitelev <max@upravitelev.de>
2021-05-05 12:43:37 +0200

    create ExhibitionInfo component

---
+++ src/components/Exhibitions/Exhibition/ExhibitionInfo.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Typography, Grid } from '@material-ui/core'
+
+
+const ExhibitionInfo = ({ exhibition }) => {
+  return (
+    <div className="app">
+    <Typography
+      variant="h3"
+      style={{
+        marginBottom: '1em',
+        backgroundColor: '#0B4F6C',
+        color: '#FBFBFF',
+      }}
+    >
+      exhibition
+    </Typography>
+    <Grid
+      container
+      // direction="row"
+      // alignItems="center"
+      // spacing={0}
+      justify="space-between"
+    >
+      <Grid item xs={4} sm={4}>
+        <img
+          src={exhibition.bannerImage}
+          alt="exhibition banner image"
+          width="300"
+        />
+        <Typography variant="body2">{exhibition.artspace}</Typography>
+        <Typography variant="h6">{exhibition.title}</Typography>
+        <Typography>
+          {exhibition.openingDay} - {exhibition.closingDay}
+        </Typography>
+      </Grid>
+      <Grid item xs={8} sm={8}>
+        <div>{exhibition.description}</div>
+      </Grid>
+    </Grid>
+  </div>
+  )
+}
+
+export default ExhibitionInfo
--- src/components/Exhibitions/Exhibition/index.js
+++ src/components/Exhibitions/Exhibition/index.js
@@ -5,12 +5,12 @@ import { useParams } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 
 // import components
-// import components
 import Capture from '../../Cam/Capture'
 import Dashboard from '../../Dashboard'
 import Infobox from '../../Infobox'
 import EndSession from './../DrivingSessions/EndSession'
 import DrivingSessions from './../DrivingSessions'
+import ExhibitionInfo from './ExhibitionInfo'
 
 // send notifications to Notification component
 import { setNotification } from '../../../reducers/notificationReducer'
@@ -57,41 +57,7 @@ const Exhibition = () => {
     return (
       <div>
         <Infobox infotext={componentsTexts.Exhibition.infotext} />
-        <div className="app">
-          <Typography
-            variant="h3"
-            style={{
-              marginBottom: '1em',
-              backgroundColor: '#0B4F6C',
-              color: '#FBFBFF',
-            }}
-          >
-            exhibition
-          </Typography>
-          <Grid
-            container
-            // direction="row"
-            // alignItems="center"
-            // spacing={0}
-            justify="space-between"
-          >
-            <Grid item xs={4} sm={4}>
-              <img
-                src={exhibition.bannerImage}
-                alt="exhibition banner image"
-                width="300"
-              />
-              <Typography variant="body2">{exhibition.artspace}</Typography>
-              <Typography variant="h6">{exhibition.title}</Typography>
-              <Typography>
-                {exhibition.openingDay} - {exhibition.closingDay}
-              </Typography>
-            </Grid>
-            <Grid item xs={8} sm={8}>
-              <div>{exhibition.description}</div>
-            </Grid>
-          </Grid>
-        </div>
+        <ExhibitionInfo exhibition={exhibition}/>
         <Grid
           container
           // direction="row"
