5403818f1454bfe6e4c2d90ea5560dda18643b5c
Max Upravitelev <max@upravitelev.de>
2021-05-11 12:12:58 +0200

    check for protocol in jitsiUrls and manipulate string if necessary

--- src/components/Stream/JitsiClient.js
+++ src/components/Stream/JitsiClient.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
-import { Jutsu } from 'react-jutsu' // Component
+import { Jutsu } from 'react-jutsu' 
 import { Button } from '@material-ui/core'
-
-
+import { useSelector } from 'react-redux'
+import { removeProtocolFromUrl } from '../../utils'
 
 const JitsiClient = () => {
-  // const [room, setRoom] = useState('')
-  // const [name, setName] = useState('')
   const [call, setCall] = useState(false)
   const [password, setPassword] = useState('')
 
+  let jitsiUrl = useSelector((state) => state.urls.jitsiUrl)
+
+  jitsiUrl = removeProtocolFromUrl(jitsiUrl)
+
   const handleClick = event => {
     event.preventDefault()
-    // if (room && name) setCall(true)
     setCall(true)
   }
 
@@ -21,7 +22,7 @@ const JitsiClient = () => {
 
   return call ? (
     <Jutsu
-      domain='meet.brimboria.net'
+      domain={jitsiUrl}
       roomName={roomName}
       displayName={displayName}
       password={password}
@@ -30,9 +31,6 @@ const JitsiClient = () => {
       errorComponent={<p>Oops, something went wrong</p>} />
   ) : (
     <form>
-      {/* <input id='room' type='text' placeholder='Room' value={room} onChange={(e) => setRoom(e.target.value)} />
-      <input id='name' type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} /> */}
-      {/* <input id='password' type='text' placeholder='Password (optional)' value={password} onChange={(e) => setPassword(e.target.value)} /> */}
       <Button 
         variant="outlined"
         onClick={handleClick} 
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const saveUrlToCache = (urlType, url) => {
 
   const loggedUserJSON = window.localStorage.getItem('loggedRoverAppUser')
-  
+
   if (loggedUserJSON) {
     const userLocal = JSON.parse(loggedUserJSON)
     userLocal.rovers[0][urlType] = url
@@ -13,7 +13,25 @@ export const saveUrlToCache = (urlType, url) => {
 export const germanDateToUSDate = (germanDate) => {
 
   let germanDateSplit = germanDate.split('.')
-  let usDate = germanDateSplit[1]+'-'+germanDateSplit[0]+'-'+germanDateSplit[2]
-  
+  let usDate = germanDateSplit[1] + '-' + germanDateSplit[0] + '-' + germanDateSplit[2]
+
   return Date.parse(usDate)
+}
+
+export const removeProtocolFromUrl = (url) => {
+
+  let tempArr = []
+  let indexAfterDoubleSlash
+
+  for (let i = 0; i < url.length; i++) {
+    if ((url[i - 1] == '/') && (url[i - 2] == '/'))
+      indexAfterDoubleSlash = i
+  }
+
+  for (let i = indexAfterDoubleSlash;i < url.length; i++) {
+      tempArr.push(url[i])
+  }
+
+  if (indexAfterDoubleSlash) return tempArr.join('')
+  else return url
 }
