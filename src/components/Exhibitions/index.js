a11afde9d22a8fd33b963ef56ee7cde6a73d2118
Max Upravitelev <max@upravitelev.de>
2021-05-21 21:04:55 +0200

    add loading div, clean up dependencies list

--- src/components/Exhibitions/index.js
+++ src/components/Exhibitions/index.js
@@ -32,7 +32,7 @@ const Exhibitions = () => {
   exhibitions.sort((a,b) => germanDateToUSDate(a.openingDay) - germanDateToUSDate(b.openingDay))
 
   // return loading info if exhibitions are not loaded yet
-  if (exhibitions.length < 1) return <div>loading...</div>
+  if (exhibitions.length < 1) return <div className={classes.elevatedDiv}>loading...</div>
 
   return (
     <div className={classes.elevatedDiv}>
